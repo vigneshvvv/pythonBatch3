2cc6a3a52c8ed671b3cedeb42ff5028c112d4bdf
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-06-13 10:29:33 +0530

    Implement insertBooks method for book insertion
    Added insertBooks method to insert book records into the database.

--- employee.py
+++ employee.py
@@ -13,7 +13,22 @@ class fetchBooks:
         conn.close()
         return data
     
+    def insertBooks(self, id, bookName, author, price):
+        conn = DataBase.get_connection()
+        cursor = conn.cursor()
+
+        query = """INSERT INTO books values (%s, %s, %s, %s)"""
+
+        values = (id, bookName, author, price)
+        cursor.execute(query, values)
+        conn.commit()
+        print("inserted successfully")
+        cursor.close()
+        conn.close()
+        return "inserted successfully"
+    
 
 
-emp = fetchBooks()
-print(emp.fetchAllData())    
+# emp = fetchBooks()
+# # print(emp.fetchAllData()) 
+# emp.insertBooks(21, "Mein kampf", "AH", 1500)   
